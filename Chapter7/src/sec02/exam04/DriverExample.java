892e9cfcd6aa5e4b2600d3d00d8c110050392205
michael85-dev <69830264+michael85-dev@users.noreply.github.com>
2021-03-30 22:40:24 +0900

    Add files via upload

---
+++ Chapter7/src/sec02/exam04/DriverExample.java
@@ -0,0 +1,18 @@
+package sec02.exam04;
+
+import sec02.exam03.*;
+
+public class DriverExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Driver driver = new Driver();
+		
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+		
+		driver.drive(bus);
+		driver.drive(taxi);
+	}
+
+}
